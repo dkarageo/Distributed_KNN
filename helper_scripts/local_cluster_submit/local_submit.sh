279ee5969628b1837ef08bec4c987d5cbdb5c6f9
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2018-01-08 16:52:23 +0200

    Updated script for local submit to cluster using qsub.
    That's the currently working version, using I2G_MPI_START mechanism.

--- helper_scripts/local_cluster_submit/local_submit.sh
+++ helper_scripts/local_cluster_submit/local_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -q see
-#PBS -N knn_16_8
+#PBS -N knn_4_8_mpich2_single
 #PBS -j oe
-#PBS -l nodes=16:ppn=8,mem=2gb
+#PBS -l nodes=4:ppn=8,mem=2gb,walltime=02:00:00
 
 #
 # local_submit.sh
@@ -23,14 +23,8 @@
 # atrributes at the beggining of this script.
 #
 
-module load openmpi-1.10-x86_64
 module load gcc/7.2.0
 
-cd $PBS_O_WORKDIR
-
-#Print info about current node.
-echo "Current cluster: " $(hostname)
-echo "Working dir: " $(pwd)
 
 ASYNC_EXECUTABLE=bin/non_blocking_knn
 BLOCKING_EXECUTABLE=bin/blocking_knn
@@ -40,11 +34,38 @@ LABELS_MODIFIER=_labels
 FILE_ENDING=.karas
 K_START=1
 K_STOP=50
+MPI_FLAVOR=MPICH2
+
+# Define the number of threads to be used by each process.
+export OMP_NUM_THREADS=8
+# Enable in order to spawn 1 process per node.
+export I2G_MPI_SINGLE_PROCESS=1
+# For processs per node <= 4, two requsted nodes may actually get allocated
+# on the same node. If that's the case, comment I2G_MPI_SIGNLE_PROCESS and
+# define explicity total number of processes.
+#export I2G_MPI_NP=8
+#export I2G_MPI_PER_NODE=2
+
+cd $PBS_O_WORKDIR
+export NP=$(cat $PBS_NODEFILE | wc -l)
+
+# Convert flavor to lowercase for passing to mpi-start.
+MPI_FLAVOR_LOWER=`echo $MPI_FLAVOR | tr '[:upper:]' '[:lower:]'`
+# Pull out the correct paths for the requested flavor.
+eval MPI_PATH=`printenv MPI_${MPI_FLAVOR}_PATH`
+# Ensure the prefix is correctly set. Don't rely on the defaults.
+eval I2G_${MPI_FLAVOR}_PREFIX=$MPI_PATH
+export I2G_${MPI_FLAVOR}_PREFIX
+
+
+#Print info about current node.
+echo "Current cluster: " $(hostname)
+echo "Working dir: " $(pwd)
 
 # Go into actual project directory. Here actual script starts from.
-cd Distributed_Knn
+cd distributed_knn
 
-make -s all
+make all
 
 # Do the same tests for async and blocking communications.
 for my_executable in $ASYNC_EXECUTABLE $BLOCKING_EXECUTABLE;
@@ -52,6 +73,11 @@ do
     echo "======================================================"
     echo "======================================================"
     echo "Executable used:" $my_executable
+
+	# Touch the executable. It exist must for the shared file system check.
+	# If it does not, then mpi-start may try to distribute the executable
+	# when it shouldn't.
+	touch $my_executable
 
     # Run tests for both normal and svd datasets.
     for datamod in "" $SVD_MODIFIER;
@@ -62,10 +88,11 @@ do
         # Run for the given range of k nearest neighbors.
         for k in $(seq $K_START $K_STOP);
         do
-            mpiexec -np $PBS_NP \
-                    ./$my_executable $DATA_FILENAME$datamod$FILE_ENDING \
-                    $DATA_FILENAME$LABELS_MODIFIER$datamod$FILE_ENDING $k
-        done
+    	    export I2G_MPI_TYPE=$MPI_FLAVOR_LOWER
+    	    export I2G_MPI_APPLICATION=$my_executable
+    	    export I2G_MPI_APPLICATION_ARGS="$DATA_FILENAME$datamod$FILE_ENDING $DATA_FILENAME$LABELS_MODIFIER$datamod$FILE_ENDING $k"
+    	    $I2G_MPI_START
+	done
 
         echo "======================================================"
     done
